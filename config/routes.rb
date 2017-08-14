427451ed83c675048fd38118328a38bc27a2be95
gsi1151 <grupo.gsi1151@gmail.com>
2017-08-14 13:49:45 -0500

    Recurso

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :pins
   get 'home/registro' => 'home#registro'
   get 'home/index' => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
