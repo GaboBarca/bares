ab5c74cecc9a104a29a34b2ccc8b0bc2ad43ed10
gsi1151 <grupo.gsi1151@gmail.com>
2017-08-17 18:04:21 -0500

    update Do

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  get 'home/registro' => 'home#registro'
+  get 'home/lugares' => 'home#lugares'
+  get 'home/tienda' => 'home#tienda'
+   devise_for :users, controllers:{
+    omniauth_callbacks: "users/omniauth_callbacks"
+
+  }
+  root 'home#index'
   get 'home/index' => 'home#index'
+    get 'home/registro' => 'home#registro'
+    
+    #mandar perticion a facebook
+    #retorna a nuestra app callback_url
+    #Procesar info de facebook
+    #Autenticar o crear nuevo usuario
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
