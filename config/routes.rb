0e7fdaf2c2a917c277198e674f5204c2b77a0572
gsi1151 <grupo.gsi1151@gmail.com>
2017-08-17 18:08:23 -0500

    update Do

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   get 'home/lugares' => 'home#lugares'
   get 'home/tienda' => 'home#tienda'
+  #root 'home#index'
+  get 'home/index' => 'home#index'
+    get 'home/registro' => 'home#registro'
    devise_for :users, controllers:{
     omniauth_callbacks: "users/omniauth_callbacks"
 
-  }
-  root 'home#index'
-  get 'home/index' => 'home#index'
-    get 'home/registro' => 'home#registro'
-    
+  }  
     #mandar perticion a facebook
     #retorna a nuestra app callback_url
     #Procesar info de facebook
